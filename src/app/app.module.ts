49df0b53bf665013c2c032d6aee943bf55f779e6
souvanny <ssisengrath@hotmail.com>
2023-05-08 20:22:51 +0200

    step 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,8 +19,10 @@ import { AccountEditComponent } from './screens/account-edit/account-edit.compon
     AllContributesComponent,
     AccountComponent,
     AccountEditComponent
+
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
--- src/app/screens/contribute/contribute.component.ts
+++ src/app/screens/contribute/contribute.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-contribute',
   templateUrl: './contribute.component.html',
   styleUrls: ['./contribute.component.css']
 })
-export class ContributeComponent {
+export class ContributeComponent implements OnInit {
+
+  answers:  { index: number, title: String }[] = [
+
+  ];
+
+  newAnswer: String = "koko";
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+
+
+  }
+
+  public validate() {
+
+    console.log("validate...");
+    console.log(this.newAnswer);
+    this.answers.push({ index: this.answers.length, title: this.newAnswer })
+
+  }
+
+  public upAnswer(index: number) {
+    if (index == 0) {
+      return;
+    }
+
+    let currentIndex: number = this.answers[index].index;
+    let otherIndex: number = this.answers[index - 1].index;
+
+    this.answers[index].index = otherIndex;
+    this.answers[index - 1].index = currentIndex;;
+
+    this.answers.sort((a, b) => {
+      return a.index - b.index < 0 ? -1 : 1;
+    });
+  }
+
+  public downAnswer(index: number) {
+    if (index == this.answers.length - 1) {
+      return;
+    }
+
+    let currentIndex: number = this.answers[index].index;
+    let otherIndex: number = this.answers[index + 1].index;
+
+    this.answers[index].index = otherIndex;
+    this.answers[index + 1].index = currentIndex;;
+
+
+    this.answers.sort((a, b) => {
+      return a.index - b.index < 0 ? -1 : 1;
+    });
+  }
+
+  public deleteAnswer(index: number) {
+
+    this.answers = this.answers.filter(item => item['index'] !== index);
+  }
 
 }
